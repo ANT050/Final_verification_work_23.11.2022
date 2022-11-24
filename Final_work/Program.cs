501ab24ce3ecd06f735992d9fde935b2c6b0cfce
Anton <tepa-86@mail.ru>
2022-11-24 08:59:06 +0300

    1. Добавление функции "Ввода номера массива" 2. Добавление вывода в консоль исходных данных

--- Final_work/Program.cs
+++ Final_work/Program.cs
@@ -4,3 +4,17 @@
 [“hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → [] */
+
+// Функция ввода номера массива
+string ArrayNumbers(string message)
+{
+	Console.Write(message);
+	return Console.ReadLine();
+}
+
+Console.WriteLine("\nМАССИВЫ:\n");
+Console.WriteLine("1 массив: [“hello”, “2”, “world”, “:-)”]");
+Console.WriteLine("2 массив: [“1234”, “1567”, “-2”, “computer science”]");
+Console.WriteLine("3 массив: [“Russia”, “Denmark”, “Kazan”]");
+
+string value = ArrayNumbers("\nУкажите номер массива: ");
